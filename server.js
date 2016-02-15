13a4c91635bf270c1af28ed0482a09ba46895cc4
Sergey Khokhlov <worldfly@yandex.ru>
2016-02-15 19:45:12 +0300

    Сделал правильный сервер

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
 const express = require('express');
 const app = express();
 
+app.set('port', process.env.PORT | 3000);
+
 app.use('/public', express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/app/app.html');
 });
 
-app.listen(3000);
+require('http').createServer(app).listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
+});
