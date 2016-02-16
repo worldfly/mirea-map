cf7422c0ec23127594ecb6ec857913a00020cee9
Sergey Khokhlov <worldfly@yandex.ru>
2016-02-16 08:53:49 +0300

    Сделал правильный сервер

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT | 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use('/public', express.static('public'));
 
