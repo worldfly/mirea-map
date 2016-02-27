9cffda5db8ade3e9f1f6c2c7a31391766920b5d3
Sergey Khokhlov <worldfly@yandex.ru>
2016-02-27 22:42:36 +0300

    Сделал прямой адрес для иконки

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-
 const staticConfig = {
     maxAge: 180000,
     index: false,
@@ -14,10 +13,8 @@ const staticConfig = {
 app.use('/public', express.static('public', staticConfig));
 app.use('/svg', express.static('svg', staticConfig));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/app/app.html');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/app/app.html'));
 
-app.listen(app.get('port'), function () {
-    console.log('Server listening on port ' + app.get('port'));
-});
+app.get('/favicon.ico', (req, res) => res.sendfile(__dirname + '/public/app/images/favicon.ico'));
+
+app.listen(app.get('port'), () => console.log('Server listening on port ' + app.get('port')));
